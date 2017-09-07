00ff532dd0a09bb4437d09858847390fc9271002
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2017-09-07 12:02:23 +0200

    added metadata for socials

--- src/components/home/about/About.js
+++ src/components/home/about/About.js
@@ -24,7 +24,7 @@ const About = () => {
                 <br/><span className="about-description">I'm a software engineer from Italy</span>
                 <br/><br/>I like to build stuff and to have fun doing it.
                 <br/>Some of my interests are: Android, UI/UX design, Web development, games development, VR and computer graphics.
-                <br/><br/>If you want to know more: <a onClick={onLinkClick} href="https://docs.google.com/document/d/1fAmPU_8BsL2XIksU9V86YQ7ea3YRO7_Da1s5N555XIU/edit?usp=sharing" target="blank"><span className="about-cv">HERE is my CV.</span></a>
+                <br/><br/>If you want to know more: <a onClick={onLinkClick} href="https://docs.google.com/document/d/1fAmPU_8BsL2XIksU9V86YQ7ea3YRO7_Da1s5N555XIU/edit?usp=sharing" target="_blank"><span className="about-cv">HERE is my CV.</span></a>
                 <br/><br/>Keep scrolling to see some of the projects I've worked on.
             </div>                
         </div>
--- src/components/home/work/projectDetails/ProjectDetails.js
+++ src/components/home/work/projectDetails/ProjectDetails.js
@@ -60,12 +60,12 @@ const ProjectDetailsCard = ({ project, onClick }) => {
                 <div className="project-details-title">{project.name}</div>
                 <div className="project-details-description"><MultilineText text={project.description} /></div>
                 <div className="project-details-main-links">{project.links.map( link => ( 
-                    <a className="project-details-link" key={link.url} href={link.url} target="blank">
+                    <a className="project-details-link" key={link.url} href={link.url} target="_blank">
                         <i className={"fa fa-" +link.icon +" project-details-icon"} aria-hidden="true" />{link.name}
                     </a> ))}
                 </div>
                 <div className="project-details-secondary-links">{project.secondaryLinks.map( link => ( 
-                    <a className="project-details-link" key={link.url} href={link.url} target="blank">
+                    <a className="project-details-link" key={link.url} href={link.url} target="_blank">
                         <i className={"fa fa-" +link.icon +" project-details-icon"} aria-hidden="true" />{link.name}
                     </a> ))}
                 </div>
