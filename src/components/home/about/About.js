71535ae6dba1b6545e9ecdf682420eed01dc8a1e
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2018-04-19 09:01:44 +0200

    changed text

--- src/components/home/about/About.js
+++ src/components/home/about/About.js
@@ -20,8 +20,8 @@ const About = () => {
         <div className="home-section container-about">        
             <img className="profile-picture-about" alt="profilepic" src={profilePic} />
             <div className="about-text">
-                <span className="about-text-main">Hello! I'm <span className="about-name">Pier</span></span>
-                <br/><span className="about-text-subtitle">I'm a software engineer from Italy</span>
+                <span className="about-text-main">Hi! I'm <span className="about-name">Pier</span></span>
+                <br/><span className="about-text-subtitle">I'm a software engineer</span>
                 <br/><br/>I like to build things with code,
                 <br/>Some of my interests are: Android, UI/UX design, Web development, games development, VR and computer graphics.
                 <br/><br/>If you want to know more: <a onClick={onLinkClick} href="https://docs.google.com/document/d/1fAmPU_8BsL2XIksU9V86YQ7ea3YRO7_Da1s5N555XIU/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><span className="about-cv">HERE is my CV.</span></a>
